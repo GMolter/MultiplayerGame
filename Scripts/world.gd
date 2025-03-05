6e6212f44df73fe5fe0cdc7c523838eb45622495
GMolter <gmolter8@gmail.com>
2025-03-05 12:35:35 -0500

    Started Map Selector

--- Scripts/world.gd
+++ Scripts/world.gd
@@ -4,6 +4,8 @@ extends Node
 @onready var address_entry =$"2D Control/MainMenu/AddressEntry"
 @onready var hud = $"2D Control/HUD"
 @onready var health_bar = $"2D Control/HUD/HealthBar"
+@onready var selector = $"2D Control/Selector"
+
 
 const Player = preload("res://Scenes/player.tscn")
 const PORT = 9999
@@ -15,7 +17,8 @@ func _unhandled_input(event):
 
 func _on_host_button_pressed():
 	main_menu.hide()
-	hud.show()
+	selector.show()
+	
 	
 	enet_peer.create_server(PORT)
 	multiplayer.multiplayer_peer = enet_peer
@@ -52,3 +55,24 @@ func update_health_bar(health_value):
 func _on_multiplayer_spawner_spawned(node):
 	if node.is_multiplayer_authority():
 		node.health_changed.connect(update_health_bar)
+
+
+
+@export var Blacksite : PackedScene
+@export var Zone47 : PackedScene
+
+var current_map: Node = null 
+
+func _on_black_site_button_pressed():
+	load_map(Blacksite)
+	selector.hide()
+	hud.show()
+	
+func _on_zone_47_button_pressed():
+	load_map(Zone47)
+
+func load_map(map_scene: PackedScene):
+	if current_map:
+		current_map.queue_free()  # Remove previous map
+	current_map = map_scene.instantiate()
+	add_child(current_map)  # Add new map to the scene tree
